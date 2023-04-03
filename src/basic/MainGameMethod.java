919c5607cece30aa40f611a933fcae84a0b51749
cononi <ekgoqhffo@gmail.com>
2023-04-03 22:31:38 +0900

    [add] 빌더 패턴 이용해서 값 표출

---
+++ src/basic/MainGameMethod.java
@@ -0,0 +1,32 @@
+package basic;
+
+public class MainGameMethod {
+
+    public static void main(String[] args) {
+        calculateScore(800, 5, 100, true);
+        MainGameBuilder mainGameBuilder = new MainGameBuilder
+                .Builder()
+                .score(800)
+                .levelCompleted(8)
+                .bonus(100)
+                .gameOver(true)
+                .build();
+        builderFinalScore(mainGameBuilder);
+    }
+
+    public static void builderFinalScore(MainGameBuilder mainGameBuilder) {
+        int finalScore = mainGameBuilder.score;
+        if(mainGameBuilder.gameOver){
+            finalScore += (mainGameBuilder.levelCompleted * mainGameBuilder.bonus);
+            System.out.println("Your Builder Final Score War " + finalScore);
+        }
+    }
+
+    public static void calculateScore(int score, int levelCompleted, int bonus, boolean gameOver) {
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score war " + finalScore);
+        }
+    }
+}
